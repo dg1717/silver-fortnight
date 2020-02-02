6a3b6c090560842d67dca958de3b0b0060ced964
gpl-git <gplagun@yahoo.com>
2020-02-02 09:49:18 -0800

    Try FF

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -28,7 +28,7 @@ public class TestContext {
     }
 
     public static void initialize() {
-        initialize("chrome", false);
+        initialize("firefox", false);
     }
     public static void teardown() {
         driver.quit();
